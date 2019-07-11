3dbdd20e7be140003cf13b89ad478d98b9951d16
W01fw00d <gabrm_91@hotmail.com>
2019-07-11 10:37:23 +0200

    Add views and templates for index and edit

--- book_generator/urls.py
+++ book_generator/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('new', views.new, name='new'),
+    path('book/<int:book_id>', views.edit, name='edit'),
 ]
--- book_generator/views.py
+++ book_generator/views.py
@@ -1,5 +1,22 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
+# from django.template import loader
+from django.shortcuts import render, get_object_or_404
+
+from .models import Book
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Book Generator. You can start a new Exquisite Corpse Book here.")
+    book_list = Book.objects.order_by('start')
+    context = {'book_list': book_list,}
+    # template = loader.get_template('book/index.html')
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'book/index.html', context)
+
+def new(request):
+    return HttpResponse("You can start a new Exquisite Corpse Book here.")
+
+def edit(request, book_id):
+    # return HttpResponse("You can edit book " + str(book_id) + " here.")
+    book = get_object_or_404(Book, pk=book_id)
+
+    return render(request, 'book/edit.html', {'book': book})
